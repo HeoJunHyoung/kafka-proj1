1d5cd2b02817a3e5f9f627bc17de95720c1f0244
junbro <gjwnsgud4016@gmail.com>
2025-09-11 15:33:40 +0900

    fix: try/catch문 수정
    - continue 위치 변경

--- practice/src/main/java/com/practice/kafka/producer/FileAppendProducer.java
+++ practice/src/main/java/com/practice/kafka/producer/FileAppendProducer.java
@@ -13,38 +13,35 @@ import java.io.File;
 import java.util.Properties;
 
 public class FileAppendProducer {
-
     public static final Logger logger = LoggerFactory.getLogger(FileAppendProducer.class.getName());
-
     public static void main(String[] args) {
-        String topicName = "file-topic"; // 토픽 이름
-        File file = new File("C:\\0. inflearn\\kafka-proj1\\practice\\src\\main\\resources\\pizza_append.txt"); // ✅ 파일 객체 생성
+        String topicName = "file-topic";
 
-        Properties props = new Properties();
+        //KafkaProducer configuration setting
+        // null, "hello world"
 
+        Properties props  = new Properties();
         props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092");
         props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        // ✅ kafkaProducer 객체 생성 -> ProducerRecord 생성 -> kafkaProducer.send() 비동기 방식 전송 -> kafkaProducer.close();
-        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+        //KafkaProducer object creation
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(props);
         boolean sync = false;
 
-        // EventHandler / FileEventSource / FileEventSourceThread 객체 생성
+        File file = new File("C:\\0. inflearn\\kafka-proj1\\practice\\src\\main\\resources\\pizza_append.txt");
         EventHandler eventHandler = new FileEventHandler(kafkaProducer, topicName, sync);
-        FileEventSource fileEventSource = new FileEventSource(1000, file, eventHandler);
-
+        FileEventSource fileEventSource = new FileEventSource(100, file, eventHandler);
         Thread fileEventSourceThread = new Thread(fileEventSource);
-        fileEventSourceThread.start();  // ✅ 계속 모니터링
+        fileEventSourceThread.start();
 
         try {
             fileEventSourceThread.join();
-        } catch (InterruptedException e) {
+        }catch(InterruptedException e) {
             logger.error(e.getMessage());
-        } finally {
+        }finally {
             kafkaProducer.close();
         }
 
     }
-
 }
